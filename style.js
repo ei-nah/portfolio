e37838f8254f0ba176b09160342c5a595a598eb6
Stephanie Bernardo <bernardo.stephanie.a@gmail.com>
2026-05-11 14:52:17 +0800

    Add javascript and change tab into spaces in tech-stack

---
+++ style.js
@@ -0,0 +1,30 @@
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+
+function openModal(src) {
+    modal.classList.remove("hidden");
+    modalImg.src = src;
+}
+
+modal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
+
+
+
+function Menu(e){
+    let list = document.querySelector('ul');
+        if (window.innerWidth < 640) {
+            if(e.name === 'menu'){
+                e.name = "close";
+                list.classList.add('top-[80px]', 'opacity-100');
+                list.classList.remove('pointer-events-none');
+        } else {
+            e.name = "menu";
+            list.classList.remove('top-[80px]', 'opacity-100');
+            list.classList.add('pointer-events-none');
+        }
+    }
+}
+
+
